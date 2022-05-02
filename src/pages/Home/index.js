84df1bb75482bc861bf227f250015329e37feda3
herbertnicolas <hna@cin.ufpe.br>
2022-05-02 14:33:39 -0300

    tmdb api working properly

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,6 +1,26 @@
+import { useEffect, useState } from "react";
+import api from "../../services/api";
+
 function Home(){
+    const [filmes, setFilmes] = useState([]);
+
+    useEffect(()=>{
+        async function loadFilmes(){
+            const response = await api.get("movie/now_playing", {
+                params:{
+                    api_key: "06d5f500bf4ae4d74d0ebdea8d156b5f",
+                    language: "pt-br",
+                    page: 1,
+                }
+            })
+            console.log(response);
+        }
+        loadFilmes();
+    },[])
     return(
-        <h1>Bem vindo a Home!</h1>
+        <div>
+            <h1>Bem vindo a Home!</h1>
+        </div>
     )
 }
 
